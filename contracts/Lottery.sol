8816bbf56689fe1ebee9316f01b2ecff25c1aac0
Clayton Auzenne <CAuzenne@discoveryed.com>
2021-05-28 15:25:41 -0400

    adding lottery contract and initial tests

---
+++ contracts/Lottery.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    modifier restricted() {
+        require(manager == msg.sender);
+        _;
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+
+        //reset the players
+        players = new address[](0);
+    }
+
+}
